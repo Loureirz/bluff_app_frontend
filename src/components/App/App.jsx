4397425e910e412b7a7384a2becb0bc749137941
Yuri Loureiro <loureirz@gmail.com>
2025-03-10 12:20:13 +0000

    add footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import "./App.css";
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <div className="page">
         <Header />
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__paragraph">© 2025 Bluff App.</p>
+        </footer>
+    );
+}
+
+export default Footer;
